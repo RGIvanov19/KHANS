b552139f9c712dd557869367ad50833c194b209d
Yuki Kompanski <iaivanov19@codingburgas.bg>
2022-02-12 14:27:05 +0200

    Make function for main menu with highlighter

--- KHANS_project/KHANS_project/Header.h
+++ KHANS_project/KHANS_project/Header.h
@@ -31,3 +31,59 @@ void gotoxy(int x, int y)
     c.Y = y;
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
+
+void optionPlay() {
+	system("cls");
+	int Set[] = { 7,7,7 };
+	int counter = 2;
+	char key;
+
+	for (int i = 0; i < 100; i++)
+	{
+		gotoxy(10, 10);
+		color(Set[0]);
+		cout << "                        Search   ";
+
+		gotoxy(10, 11);
+		color(Set[1]);
+		cout << "                        Add event   ";
+
+		gotoxy(10, 12);
+		color(Set[2]);
+		cout << "                        Exit   ";
+
+		key = _getch();
+
+		if (key == 72 && (counter >= 2 && counter <= 3)) {
+			counter--;
+		}
+		if (key == 80 && (counter >= 1 && counter <= 2)) {
+			counter++;
+		}
+		if (key == '\r') {
+			if (counter == 1) {
+				//tuk dobavqsh funckiite
+			}
+			if (counter == 2) {
+				//tuk dobavqsh funckiite
+			}
+			if (counter == 3) {
+				break;
+			}
+		}
+		Set[0] = 7;
+		Set[1] = 7;
+		Set[2] = 7;
+
+		if (counter == 1) {
+			Set[0] = 12;
+		}
+		if (counter == 2) {
+			Set[1] = 12;
+		}
+		if (counter == 3) {
+			Set[2] = 12;
+		}
+
+	}
+}
