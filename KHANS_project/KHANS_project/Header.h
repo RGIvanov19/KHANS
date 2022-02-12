bbf828e945925c1e35d228a3a0acd5914a0eec9f
Yuki Kompanski <iaivanov19@codingburgas.bg>
2022-02-12 14:25:38 +0200

    Make function to position text

--- KHANS_project/KHANS_project/Header.h
+++ KHANS_project/KHANS_project/Header.h
@@ -24,4 +24,10 @@ void color(int color) {
     SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 }
 
-
+void gotoxy(int x, int y)
+{
+    COORD c;
+    c.X = x;
+    c.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+}
