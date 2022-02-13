2b0237eabcb51668cc4f806eddcc8aadd762316f
YGDimitrov19 <YGDimitrov19@codingburgas.bg>
2022-02-13 19:40:44 +0200

    add Q&A documentation and unittests

--- KHANS_project/UNITTEST/UNITTEST.cpp
+++ KHANS_project/UNITTEST/UNITTEST.cpp
@@ -5,22 +5,60 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace UNITTEST
 {
-	TEST_CLASS(UNITTEST)
-	{
-	public:
-		
-            BEGIN_TEST_METHOD_ATTRIBUTE(expectToReturnDecToBin)
+    TEST_CLASS(UNITTEST)
+    {
+    public:
+
+        BEGIN_TEST_METHOD_ATTRIBUTE(expectToReturnDecToBin)
             TEST_OWNER(L"YGDimitrov19")
             TEST_PRIORITY("Low")
             END_TEST_METHOD_ATTRIBUTE()
             TEST_METHOD(expectToReturnDecToBin)
         {
             int n = 5;
-           
 
-            bool result = numToGray(n);
+
+            bool result = decToBin(n);
 
             Assert::AreEqual(true, result);
         }
-	};
+
+        BEGIN_TEST_METHOD_ATTRIBUTE(expectToReturnDecToBinWithZero)
+            TEST_OWNER(L"YGDimitrov19")
+            TEST_PRIORITY("Low")
+            END_TEST_METHOD_ATTRIBUTE()
+            TEST_METHOD(expectToReturnDecToBinWithZero)
+        {
+            int n = 0;
+
+
+            bool result = decToBin(n);
+
+            Assert::AreEqual(false, result);
+        }
+        BEGIN_TEST_METHOD_ATTRIBUTE(expectToReturnDecToBinWithNegativeNumber)
+            TEST_OWNER(L"YGDimitrov19")
+            TEST_PRIORITY("Low")
+            END_TEST_METHOD_ATTRIBUTE()
+            TEST_METHOD(expectToReturnDecToBinWithNegativeNumber)
+        {
+            int n = -11;
+
+
+            bool result = decToBin(n);
+
+            Assert::AreEqual(true, result);
+        }
+
+
+
+
+
+
+    };
 }
+
+
+
+
+
